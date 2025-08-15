13451d849aafccdf63cf1c7be06e6683c020d02a
rbentes <rbntes@gmail.com>
2025-08-15 09:34:44 -0400

    Fluxo de redirecionamento para usuario logado e deslogado

--- www/dmxAppConnect/config.js
+++ www/dmxAppConnect/config.js
@@ -6,5 +6,45 @@ dmx.config({
         "type": "number"
       }
     ]
+  },
+  "home": {
+    "localStorage": [
+      {
+        "type": "number",
+        "name": "id"
+      }
+    ],
+    "usuario_logado": {
+      "meta": {},
+      "local": [
+        {
+          "name": "",
+          "type": "boolean",
+          "metaData": {
+            "condition": {}
+          }
+        }
+      ]
+    }
+  },
+  "backoffice": {
+    "localStorage": [
+      {
+        "type": "number",
+        "name": "id"
+      }
+    ],
+    "redirect_deslogado": {
+      "meta": {},
+      "local": [
+        {
+          "name": "",
+          "type": "boolean",
+          "metaData": {
+            "condition": {}
+          }
+        }
+      ]
+    }
   }
 });
